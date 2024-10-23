3562e17ad9fd8f0f30f941be57f31336e7d72438
dimakisiv <dimakisiv@gmail.com>
2024-10-23 16:23:51 +0300

    завдання 1 виконано

---
+++ task1.py
@@ -0,0 +1,25 @@
+import pulp
+#створюємо модель
+model = pulp.LpProblem("Maximize_Production", pulp.LpMaximize)
+
+#змінні які потрібно визначити
+lemonade = pulp.LpVariable('lemonade', lowBound=0, cat='Integer')  
+juice = pulp.LpVariable('juice', lowBound=0, cat='Integer') 
+
+#задаємо обмеження
+model += 2 * lemonade + juice <= 100
+model += 1 * lemonade <= 50
+model += 1 * lemonade <= 30
+model += 1 * lemonade <= 40
+model += 2 * juice <= 40
+
+#задаємо цільову функцію
+model += lemonade + juice, "Maximize_Production"
+
+#розв'язання
+model.solve()
+
+print("Виробляти лимонадів:", lemonade.varValue)
+print("Виробляти соків:", juice.varValue)
+print("Максимум напоїв:", lemonade.varValue + juice.varValue)
+
